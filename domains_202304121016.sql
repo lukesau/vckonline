d74500e44ddf5513b4aa6c4e0cac45ecf00ad858
lukesau <lukesau@gmail.com>
2023-04-12 10:16:43 -0700

    added database creation scripts

---
+++ domains_202304121016.sql
@@ -0,0 +1,17 @@
+INSERT INTO vckonline.domains (name,gold_cost,shadow_count,holy_count,soldier_count,worker_count,vp_reward,has_activation_effect,has_passive_effect,passive_effect,activation_effect,`text`,image) VALUES
+	 ('Jousting Field',13,1,0,1,1,3,0,1,NULL,NULL,'During your Harvest Phase, gain 1gp * Knight you own.',NULL),
+	 ('Ancient Tomb',7,0,1,1,1,3,1,0,NULL,NULL,'Immediately add 3sp to a Monster Strength value.',NULL),
+	 ('Foxgrove Palisade',9,1,0,1,0,3,0,1,NULL,NULL,'During your Roll Phase, you may pay 2gp to change one die to equal 6.',NULL),
+	 ('The Desert Orchid',9,1,1,0,0,3,0,1,NULL,NULL,'During your Roll Phase, you may pay 1gp * owned Holy Citizens to change one die to equal 1.',NULL),
+	 ('Pretorius Conclave',8,1,1,1,1,2,1,0,NULL,NULL,'Immediately gain a Citizen from the center stacks.',NULL),
+	 ('Emerald Stronghold',12,0,1,1,1,5,0,1,NULL,NULL,'During your Action phase, ignore ''+'' when buying Citizens.',NULL),
+	 ('Pratchett''s Plateau',8,0,1,0,1,3,0,1,NULL,NULL,'During your Action phase, Domains cost you 1gp less to buy.',NULL),
+	 ('Shelly Commons',13,0,1,1,1,4,0,1,NULL,NULL,'At the end of your Action phase, pay 1gp to a Player of your choice to gain 1vp.',NULL),
+	 ('Cathedral of St Aquila',8,0,2,0,0,3,0,1,NULL,NULL,'At the end of your Action phase, take 1gp from a Player of your choice.',NULL),
+	 ('Cursed Cavern',10,1,1,0,1,2,1,0,NULL,NULL,'All players immediately flip a Citizen and you gain 4mp.',NULL);
+INSERT INTO vckonline.domains (name,gold_cost,shadow_count,holy_count,soldier_count,worker_count,vp_reward,has_activation_effect,has_passive_effect,passive_effect,activation_effect,`text`,image) VALUES
+	 ('Darktide Harbour',6,1,0,1,1,2,1,0,NULL,NULL,'Immediately gain a Shadow Citizen from the center stacks.',NULL),
+	 ('King Tower',12,0,1,0,2,3,0,1,NULL,NULL,'At the end of your Action Phase, pay 1mp to a Player of your choice to gain 1vp.',NULL),
+	 ('Cloudrider''s Camp',8,0,1,1,0,2,1,0,NULL,NULL,'Immediately gain 3sp and a Soldier Citizen worth 2gp or less.',NULL),
+	 ('The Orb of Urdr',6,1,1,0,0,1,0,1,NULL,NULL,'At the end of your Action Phase, take 1mp from a Player of your choice.',NULL),
+	 ('Wisborg',6,1,0,0,2,3,1,0,NULL,NULL,'You may immediately pay 3gp to gain 3vp.',NULL);
---
+++ monsters_202304121016.sql
@@ -0,0 +1,39 @@
+INSERT INTO vckonline.monsters (name,area,`type`,`order`,strength_cost,magic_cost,vp_reward,gold_reward,strength_reward,magic_reward,has_special_reward,special_reward,has_special_cost,special_cost,is_extra,image) VALUES
+	 ('Goblin','Hills','Minion',1,1,0,1,1,0,0,0,NULL,NULL,NULL,0,NULL),
+	 ('Goblin','Hills','Minion',2,1,0,1,1,0,0,0,NULL,NULL,NULL,0,NULL),
+	 ('Goblin','Hills','Minion',3,1,0,1,1,0,0,0,NULL,NULL,NULL,0,NULL),
+	 ('Goblin Mage','Hills','Titan',4,3,0,2,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Goblin Bomber','Hills','Warden',6,5,0,3,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Goblin King','Hills','Boss',7,6,0,4,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Goblin Mage','Hills','Titan',5,3,0,2,0,0,0,1,NULL,NULL,NULL,1,NULL),
+	 ('Skeleton','Ruins','Minion',1,2,0,2,1,0,0,0,NULL,NULL,NULL,0,NULL),
+	 ('Skeleton','Ruins','Minion',2,2,0,2,1,0,0,0,NULL,NULL,NULL,0,NULL),
+	 ('Skeleton','Ruins','Minion',3,2,0,2,1,0,0,0,NULL,NULL,NULL,0,NULL);
+INSERT INTO vckonline.monsters (name,area,`type`,`order`,strength_cost,magic_cost,vp_reward,gold_reward,strength_reward,magic_reward,has_special_reward,special_reward,has_special_cost,special_cost,is_extra,image) VALUES
+	 ('Flaming Skeleton','Ruins','Titan',4,5,0,3,1,0,1,0,NULL,NULL,NULL,0,NULL),
+	 ('Flaming Skeleton','Ruins','Titan',5,5,0,3,1,0,1,0,NULL,NULL,NULL,1,NULL),
+	 ('Death Knight','Ruins','Warden',6,7,0,1,0,0,5,0,NULL,NULL,NULL,0,NULL),
+	 ('Skeleton King','Ruins','Boss',7,8,0,4,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Treant','Forest','Minion',1,3,0,1,1,0,1,0,NULL,NULL,NULL,0,NULL),
+	 ('Treant','Forest','Minion',2,3,0,1,1,0,1,0,NULL,NULL,NULL,0,NULL),
+	 ('Treant','Forest','Minion',3,3,0,1,1,0,1,0,NULL,NULL,NULL,1,NULL),
+	 ('Bane Spider','Forest','Beast',4,6,0,2,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Bane Spider','Forest','Beast',5,6,0,2,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Ettercap','Forest','Warden',6,3,5,2,0,0,0,1,NULL,NULL,NULL,0,NULL);
+INSERT INTO vckonline.monsters (name,area,`type`,`order`,strength_cost,magic_cost,vp_reward,gold_reward,strength_reward,magic_reward,has_special_reward,special_reward,has_special_cost,special_cost,is_extra,image) VALUES
+	 ('Spider Queen','Forest','Boss',7,10,3,5,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Owlbear','Valley','Beast',1,4,0,2,0,0,2,0,NULL,NULL,NULL,0,NULL),
+	 ('Owlbear','Valley','Beast',2,4,0,2,0,0,2,0,NULL,NULL,NULL,0,NULL),
+	 ('Owlbear','Valley','Beast',3,4,0,2,0,0,2,0,NULL,NULL,NULL,0,NULL),
+	 ('Giant','Valley','Titan',4,8,0,3,1,0,2,0,NULL,NULL,NULL,0,NULL),
+	 ('Giant','Valley','Titan',5,8,0,3,1,0,2,0,NULL,NULL,NULL,1,NULL),
+	 ('Satyr Mage','Valley','Warden',6,5,5,3,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Troll','Valley','Boss',7,12,0,6,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Dire Bear','Mountains','Beast',1,5,0,3,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Dire Bear','Mountains','Beast',2,5,0,3,0,0,0,1,NULL,NULL,NULL,0,NULL);
+INSERT INTO vckonline.monsters (name,area,`type`,`order`,strength_cost,magic_cost,vp_reward,gold_reward,strength_reward,magic_reward,has_special_reward,special_reward,has_special_cost,special_cost,is_extra,image) VALUES
+	 ('Dire Bear','Mountains','Beast',3,5,0,3,0,0,0,1,NULL,NULL,NULL,1,NULL),
+	 ('Orc Warrior','Mountains','Minion',4,9,0,3,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Orc Warrior','Mountains','Minion',5,9,0,3,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Orc Batrider','Mountains','Warden',6,12,0,5,0,0,0,1,NULL,NULL,NULL,0,NULL),
+	 ('Orc Chieftain','Mountains','Boss',7,14,0,6,0,0,0,1,NULL,NULL,NULL,0,NULL);
